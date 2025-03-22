e7738898ca95de39bec1797d3c654b5af89ad532
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-03-22 03:03:42 +0000

    Your commit message

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# sanatanimuisc
+# 𝗠𝗨𝗦𝗜𝗖 𝗠𝗔𝗡𝗔𝗚𝗘𝗠𝗘𝗡𝗧 𝗕𝗢𝗧 🎵
+
+<p align="center">⚡ 𝗔 𝗣𝗢𝗪𝗘𝗥𝗙𝗨𝗟 𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧 𝗙𝗢𝗥 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 ⚡</p>
+
+<h1 align="center">🇮🇳 𝗝𝗔𝗬 𝗛𝗜𝗡𝗗, 𝗝𝗔𝗬 𝗕𝗛𝗔𝗥𝗔𝗧 🇮🇳</h1>
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+  <img src="https://readme-typing-svg.herokuapp.com?color=FF0085&width=620&lines=🇮🇳+𝗣𝗢𝗪𝗘𝗥𝗘𝗗+𝗕𝗬+𝗧𝗘𝗔𝗠+𝗭𝗬𝗥𝗢+🇮🇳">
+  <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+</p>
+
+<h1 align="center"><b>𝗧𝗘𝗔𝗠 𝗭𝗬𝗥𝗢 𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧</b></h1>
+
+<p align="center">
+  <a href="https://t.me/sukuna_Dev">
+    <img src="https://envs.sh/fbk.jpg" width="400">
+  </a>
+</p>
+
+---
+
+<h3 align="center">🚀 ᴅᴇᴘʟᴏʏ 𝗢𝗡 𝗛𝗘𝗥𝗢𝗞𝗨 🚀</h3>
+
+<p align="center">
+  <a href="https://dashboard.heroku.com/new?template=https://github.com/TeamZyro/ZYRO">
+    <img src="https://img.shields.io/badge/Deploy%20On%20Heroku-blue?style=for-the-badge&logo=Heroku" width="220" height="38.45"/>
+  </a>
+</p>
+
+---
+
+![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=TeamZyro&repo=Zyro&theme=flag-india)
+
+---
+
+<h3 align="center">🌟 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 🌟</h3>
+
+<p align="center">
+  <a href="https://t.me/PiratesMainchat">
+    <img src="https://img.shields.io/badge/-Support%20Group-blue.svg?style=for-the-badge&logo=Telegram">
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://t.me/TEAMZYRO">
+    <img src="https://img.shields.io/badge/-Support%20Group-blue.svg?style=for-the-badge&logo=Telegram">
+  </a>
+</p>
+
+---
+
+- **Special Thanks to [THE TEAM ZYRO](https://github.com/TeamZyro) for [𝖳HE NGUESS BOT](https://github.com/TeamZyro/Nguess) 🎶**  
+
+### This is a Public **Music Management Bot** made by:  
+- [Xeno Kakarot](https://t.me/Xeno_Kakarot)  
+- [Ryomen Sukuna](https://t.me/sukuna_Dev)
+- 
---
+++ config.py
@@ -0,0 +1,190 @@
+import re
+from os import getenv
+# ------------------------------------
+# ------------------------------------
+from dotenv import load_dotenv
+from pyrogram import filters
+# ------------------------------------
+# ------------------------------------
+load_dotenv()
+# ------------------------------------
+# -----------------------------------------------------
+API_ID = getenv("API_ID", "24965086")
+API_HASH = getenv("API_HASH", "b9c764ce47c010e1a887f19fea54f648")
+
+EVAL = list(map(int, getenv("EVAL", "6301302395 7638720582").split()))
+# ------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", "7245575969:AAHJePj-eYF050bfAV3K1MwMWVf-ztEY3Uc")
+# -------------------------------------------------------
+OWNER_USERNAME = getenv("OWNER_USERNAME","Orewa_KANISH")
+# --------------------------------------------------------
+BOT_USERNAME = getenv("BOT_USERNAME" , "Alisa_Kujou_Music_Bot")
+# --------------------------------------------------------
+BOT_NAME = getenv("BOT_NAME" , "alisa")
+# ---------------------------------------------------------
+ASSUSERNAME = getenv("ASSUSERNAME" , "Alisa_Kujou")
+# ---------------------------------------------------------
+
+
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = getenv("DB_NAME", "Zyro")
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+
+# ----------------------------------------------------------------
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+LOGGER_ID = int(getenv("LOGGER_ID", -1002056007523))
+CHARA_CHANNEL_ID = getenv("CHARA_CHANNEL_ID", "-1002051009747")
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+OWNER_ID = int(getenv("OWNER_ID", 6382664842))
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+# ----------------------------------------------------------------
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/TeamZyro/sanatanimuisc",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # ----------------------------------------------------------------
+# -------------------------------------------------------------------
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SANATANI_X_PFP")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+TTGzd3lYNMIxMDA1")
+# ------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+
+
+
+
+
+
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "9000"))
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", "9999999"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999"))
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "1c21247d714244ddbb09925dac565aed")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b58200860623ef19")
+# ----------------------------------------------------------------------------------
+
+
+
+
+# -----------------------------------------------------------------------------------
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
+# --------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+
+
+
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+STRING1 = getenv("STRING_SESSION", "BQGP6NEAUWWY5puBlZ8kOMRkQpz_GW1Kl-4VdTg3SBrfXOuj1HsJUP96z58lANfDSHd8VJXVEvR9bbHuAleFXXmQzf1uUxPvnxztqFQPjVKQfnKjvL-QPyAWBpGdSTPox4UsO8n2HlnXcr7DyT3N2nAo5Ayl94v2ULGOn5crqiIszlCYJape0oTOD4x5PiDtRkJVrXZ-EQPbFcL-GNFS4yigD5WK10trdRM1CDMcAFVm3JdVhLYEeqp5hiqwX7fAfvhxqNFSdDoH1LHZS45QBTsEg3LQPPDrmSDQ2ZsEsgp1Ky3cHxJYmG2-rDzF44z1p9e6_2vycIY_ac9ye8CPmSrmS7WHywAAAAGtDvQlAA")
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+STRING6 = getenv("STRING_SESSION6", None)
+STRING7 = getenv("STRING_SESSION7", None)
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://files.catbox.moe/bcdx63.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://files.catbox.moe/bct8lb.jpg"
+)
+PLAYLIST_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/bct8lb.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/bct8lb.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/bct8lb.jpg"
+
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_CHAT:
+    if not re.match("(?:http|https)://", SUPPORT_CHAT):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+        )
+# ---------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
